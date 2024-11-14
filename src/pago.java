b79b3f17f8fb4b5584468e2fa6197fa49c08c331
A-nDr32-O <ac7143589@gmail.com>
2024-11-14 16:00:48 -0500

    Cambios

--- src/pago.java
+++ src/pago.java
@@ -6,8 +6,27 @@ class Pago implements IPago {
     private double monto;
     private String metodo;
 
+    public Pago() {
+    }
+
     public Pago(double monto, String metodo) {
         this.monto = monto;
+        this.metodo = metodo;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getMetodo() {
+        return metodo;
+    }
+
+    public void setMetodo(String metodo) {
         this.metodo = metodo;
     }
 
